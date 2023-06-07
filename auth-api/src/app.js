80e8d1e88d31dfadc39b1d4ad399f91a6a964289
Joaonorr <joaonoroes.jn@gmail.com>
2023-06-07 09:09:12 -0300

    feat: add app.js into src

---
+++ auth-api/src/app.js
@@ -0,0 +1,13 @@
+import express from 'express';
+
+const app = express();
+const env = process.env;
+const PORT = env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
