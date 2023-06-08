eb1efa8954e8466bb7b6e46e1e7ef0deb4914559
Joaonorr <joaonoroes.jn@gmail.com>
2023-06-08 15:18:13 -0300

    feat: add auth config to connection postgres

---
+++ auth-api/src/config/initialData.config.js
@@ -0,0 +1,25 @@
+import bcrypt from 'bcrypt';
+import User from '../models/user.models.js';
+
+export const CreateInitialData = async () => {
+    console.log('Creating initial data...');
+
+    await User.sync({ force: true });
+
+    let password = await bcrypt.hash('123456', 10);
+
+    let newUser = {
+        userName: 'Admin',
+        email: 'admin@admin.com',
+        password: password
+    };
+
+    try {
+        await User.create(newUser);
+    }
+    catch (error) {
+        console.log(error);
+    }
+}
+
+// export default CreateInitialData;
