319283a0b8f9eccbd046e48b5b3bdbb7196bf82d
Joaonorr <joaonoroes.jn@gmail.com>
2023-06-08 15:20:23 -0300

    feat: add InitialData to app.js

--- auth-api/src/app.js
+++ auth-api/src/app.js
@@ -1,8 +1,11 @@
 import express from 'express';
+import * as db from './config/initialData.config.js'
 
 const app = express();
 const env = process.env;
 const PORT = env.PORT || 8082;
+
+db.CreateInitialData();
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
