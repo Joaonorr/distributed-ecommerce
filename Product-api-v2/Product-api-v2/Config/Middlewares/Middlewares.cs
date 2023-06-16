dcf8ad254ac5b13a6e7f6d81d79f77d061fd9494
Joaonorr <joaonoroes.jn@gmail.com>
2023-06-16 17:41:32 -0300

    feat: add config to services and middlewares

---
+++ Product-api-v2/Product-api-v2/Config/Middlewares/Middlewares.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.Data.SqlClient;
+
+namespace Product_api_v2.Config.ConfigureMiddlewares;
+
+public static class Middlewares
+{
+    public static void MiddlewaresConfig(this WebApplication app)
+    {
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+
+        //app.UseExceptionHandler("/error");
+        //app.Map("/error", (HttpContext http) =>
+        //{
+
+        //    var error = http.Features?.Get<IExceptionHandlerFeature>()?.Error;
+
+        //    if (error != null)
+        //    {
+        //        if (error is SqlException)
+        //            return Results.Problem(title: "Database out", statusCode: 500);
+        //        else if (error is BadHttpRequestException)
+        //            return Results.Problem(title: "Error to convert data to other type. See all the information sent", statusCode: 500);
+        //    }
+
+        //    return Results.Problem(title: "An error ocurred", statusCode: 500);
+        //});
+    }
+}
