4f14582c8a41ceccc753edf9c60b255298e1fe7a
Joaonorr <joaonoroes.jn@gmail.com>
2023-06-16 17:38:40 -0300

    feat: create product get all endpoint

---
+++ Product-api-v2/Product-api-v2/Endpoints/Product/ProductGetAll.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Product_api_v2.Data;
+
+namespace Product_api_v2.Endpoints.Product;
+
+public static class ProductGetAll
+{
+    public static string Template => "/product";
+    public static string[] Method => new string[] { HttpMethod.Get.ToString() };
+    public static Delegate Handle => Action;
+
+    public static async Task<IResult> Action(ApplicationDbContext context)
+    {
+        var products = context.Products
+            .Include(p => p.Name)
+            .ToList();
+
+        return Results.Ok(products);
+    }
+}
