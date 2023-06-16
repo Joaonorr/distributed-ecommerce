026b14d7023b8d2d3eb803fcba1281b0546998f3
Joaonorr <joaonoroes.jn@gmail.com>
2023-06-16 17:39:12 -0300

    feat: create route to product

---
+++ Product-api-v2/Product-api-v2/Routes/Product.cs
@@ -0,0 +1,11 @@
+﻿using Product_api_v2.Endpoints.Product;
+
+namespace Product_api_v2.Routes;
+
+public static class Product
+{
+    public static void ProductRoutes(this WebApplication app)
+    {
+        app.MapMethods(ProductGetAll.Template, ProductGetAll.Method, ProductGetAll.Handle);
+    }
+}
